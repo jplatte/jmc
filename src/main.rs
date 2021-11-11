fecf03ecd09c9f5ac4d2d65cf4d8e840ffc6ba33
Jonas Platte <jplatte+git@posteo.de>
2021-11-11 20:45:58 +0100

    We needz async

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use std::{thread, time::Duration};
-
 use druid::{AppLauncher, PlatformError, Selector, Target, WindowDesc};
+use tokio::time::{self, Duration};
 use tracing::error;
 
 mod data;
@@ -10,19 +9,20 @@ pub use data::{AppState, LoginState, UserState};
 
 const FINISH_LOGIN: Selector<()> = Selector::new("finish-login");
 
-fn main() -> Result<(), PlatformError> {
+#[tokio::main]
+async fn main() -> Result<(), PlatformError> {
     tracing_subscriber::fmt::init();
 
     let launcher = AppLauncher::with_window(WindowDesc::new(ui::build_ui()));
 
     let event_sink = launcher.get_external_handle();
-    thread::spawn(move || switch_view_soon(event_sink));
+    tokio::spawn(switch_view_soon(event_sink));
 
     launcher.launch(AppState::default())
 }
 
-fn switch_view_soon(event_sink: druid::ExtEventSink) {
-    thread::sleep(Duration::from_secs(3));
+async fn switch_view_soon(event_sink: druid::ExtEventSink) {
+    time::sleep(Duration::from_secs(3)).await;
 
     if let Err(e) = event_sink.submit_command(FINISH_LOGIN, (), Target::Auto) {
         error!("{}", e);
