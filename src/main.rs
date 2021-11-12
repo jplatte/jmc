cdc0acf78b212b0e990ebfd35b204d0530fc8cf0
Jonas Platte <jplatte+git@posteo.de>
2021-11-12 01:02:55 +0100

    Non-async main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use druid::{AppLauncher, PlatformError, Selector, Target, WindowDesc};
-use tokio::time::{self, Duration};
+use tokio::{
+    runtime::Runtime,
+    time::{self, Duration},
+};
 use tracing::error;
 
 mod data;
@@ -9,16 +12,25 @@ pub use data::{AppState, LoginState, UserState};
 
 const FINISH_LOGIN: Selector<()> = Selector::new("finish-login");
 
-#[tokio::main]
-async fn main() -> Result<(), PlatformError> {
+fn main() -> Result<(), PlatformError> {
     tracing_subscriber::fmt::init();
+
+    // Create a runtime and have it register its threadlocal magic.
+    // The main thread will not block on a future like in most async
+    // applications because it will run the GUI instead, which is not async.
+    let rt = Runtime::new().unwrap();
+    let _guard = rt.enter();
 
     let launcher = AppLauncher::with_window(WindowDesc::new(ui::build_ui()));
 
     let event_sink = launcher.get_external_handle();
     tokio::spawn(switch_view_soon(event_sink));
 
-    launcher.launch(AppState::default())
+    launcher.launch(AppState::default())?;
+
+    // After the GUI is closed, shut down all pending async tasks.
+    rt.shutdown_timeout(Duration::from_secs(5));
+    Ok(())
 }
 
 async fn switch_view_soon(event_sink: druid::ExtEventSink) {
